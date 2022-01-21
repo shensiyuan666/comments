16c8c70ec67f8614ad890ed4a529fb7e178a9ad4
shensiyuan <302523363@qq.com>
2022-01-21 13:14:05 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,87 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-input type="textarea" rows="10" placeholder="请输入评论内容"  v-model="comment"></el-input>
+    <el-button @click="addComment">添加评论</el-button>
+    <h3>评论列表</h3>
+    <el-button style="marginBottom:30px" v-show="this.commentList.length" @click="isShowAll=!isShowAll">{{this.isShowAll?'折叠':'展开'}}所有评论</el-button>
+    {{this.commentList.length?'':'暂无评论，请添加第一条评论'}}
+    <div v-for="item in commentList" :key="item.id" style="marginBottom:30px" v-show="isShowAll">
+      <el-button @click="toggle(item.id)">{{item.isShow?'折叠':'展开'}}评论</el-button>
+      <div v-show="item.isShow">
+        <div>{{item.content}}</div>
+        <a href="javascript:;" @click="showInput(item.id)">回复</a>
+        <el-button @click="toggle1(item.id)" v-show="item.replyList.length">{{item.isShowReply?'折叠':'展开'}}回复</el-button>
+        <div v-for="item1 in item.replyList" :key="item1.id" v-show="item.isShowReply">{{item1.content}}</div>
+        <div v-show="item.isShowInput" style="display:flex">
+          <el-input v-model="item.reply"></el-input>
+          <el-button @click="addReply(item.id)">发送</el-button>
+          <el-button @click="hiddenInput(item.id)">取消</el-button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      comment:'',
+      commentList:[],
+      isShowAll:true
+    }
+  },
+  created(){
+    let res=window.sessionStorage.getItem('commentList')
+    this.commentList=JSON.parse(res)===null?[]:JSON.parse(res)
+  },
+  methods:{
+    toggle(id){
+      let index=this.commentList.findIndex(v=>v.id===id)
+      this.commentList[index].isShow=!this.commentList[index].isShow
+    },
+    addComment(){
+      if(this.comment.trim().length===0){
+          this.$message({
+          type:'error',
+          message: '评论不能为空！'
+        })
+      }
+      else{
+        let id=this.commentList.length===0?0:this.commentList[0].id+1
+        this.commentList.unshift({id:id,content:this.comment,isShow:true,isShowInput:false,replyList:[],      reply:'',isShowReply:true})
+        window.sessionStorage.setItem('commentList',JSON.stringify(this.commentList))
+        this.comment=''
+      }
+    },
+    showInput(id){
+      let index=this.commentList.findIndex(v=>v.id===id)
+      this.commentList[index].isShowInput=true
+    },
+    addReply(id){
+      let index=this.commentList.findIndex(v=>v.id===id)
+      let rid=this.commentList[index].replyList.length===0 ? 0 : this.commentList[index].replyList[this.commentList[index].replyList.length-1].rid+1
+      let reply=this.commentList[index].reply
+      this.commentList[index].replyList.push({rid:rid,content:reply})
+      this.commentList[index].isShowInput=false
+      window.sessionStorage.setItem('commentList',JSON.stringify(this.commentList))
+      this.commentList[index].reply=''
+    },
+    hiddenInput(id){
+      let index=this.commentList.findIndex(v=>v.id===id)
+      this.commentList[index].isShowInput=false
+      this.commentList[index].reply=''
+    },
+    toggle1(id){
+      let index=this.commentList.findIndex(v=>v.id===id)
+      this.commentList[index].isShowReply=!this.commentList[index].isShowReply
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import {Input,Button,Message} from 'element-ui'
 
 Vue.config.productionTip = false
+
+Vue.use(Input)
+Vue.use(Button)
+Vue.use(Message)
+
+Vue.prototype.$message = Message
 
 new Vue({
   render: h => h(App),
